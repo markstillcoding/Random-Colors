c40f5542ed66a676b8dd7721fff94de42e0ac68e
Mark Cates <cates@Marks-iMac.home>
2026-06-15 16:45:32 -0500

    added input range sliders and collected numbers on input and placed in object for use in creating new random colors

--- script.js
+++ script.js
@@ -1,47 +1,50 @@
-const changeColorEl = document.querySelector('.app__btn')
-const colorBoxEl = document.querySelector('.app__color-box');
-const customColorEl = document.querySelector('.custom-color');
+const changeColorEl = document.querySelector(".app__btn");
+const colorBoxEl = document.querySelector(".app__color-box");
+const customColorEl = document.querySelector(".custom-color");
 
 
 const generatedColors = {
   rColors: [],
   gColors: [],
   bColors: [],
-  sameColors: []
-}
+  sameColors: [],
+};
 
 function createNewColor() {
-  let r = Math.floor(Math.random() * 255) + 1
-  let g = Math.floor(Math.random() * 255) + 1
-  let b = Math.floor(Math.random() * 255) + 1
+  let r = Math.floor(Math.random() * 255) + 1;
+  let g = Math.floor(Math.random() * 255) + 1;
+  let b = Math.floor(Math.random() * 255) + 1;
 
   let newColor = {
     r,
     g,
-    b
-  }
+    b,
+  };
 
-  return newColor
+  return newColor;
 }
 
 function createChosenColor(color) {
-  let r = Math.floor(Math.random() * (color.red[1] - color.red[0]) + color.red[0]);
-  let g = Math.floor(Math.random() * (color.green[1] - color.green[0]) + color.green[0]);
-  let b = Math.floor(Math.random() * (color.blue[1] - color.blue[0]) + color.blue[0]);
+  let r = Math.floor(
+    Math.random() * (color.red[1] - color.red[0]) + color.red[0],
+  );
+  let g = Math.floor(
+    Math.random() * (color.green[1] - color.green[0]) + color.green[0],
+  );
+  let b = Math.floor(
+    Math.random() * (color.blue[1] - color.blue[0]) + color.blue[0],
+  );
 
   let newColor = {
     r,
     g,
-    b
-  }
+    b,
+  };
 
-  return newColor
+  return newColor;
 }
 
-
-
-
-let i = 1
+let i = 1;
 
 function analyzeColor(newColors) {
   let color = null;
@@ -52,64 +55,57 @@ function analyzeColor(newColors) {
   }
   setTimeout(
     () => {
-
       formatNewColor(color);
       if (color.r === color.g && color.r > color.b) {
-        generatedColors.rColors.push(color)
+        generatedColors.rColors.push(color);
       } else if (color.r === color.b && color.r > color.g) {
-        generatedColors.rColors.push(color)
+        generatedColors.rColors.push(color);
       } else if (color.b === color.g && color.b > color.r) {
-        generatedColors.bColors.push(color)
+        generatedColors.bColors.push(color);
       } else if (color.r > color.g && color.r > color.b) {
-        generatedColors.rColors.push(color)
+        generatedColors.rColors.push(color);
       } else if (color.g > color.b && color.g > color.r) {
-        generatedColors.gColors.push(color)
+        generatedColors.gColors.push(color);
       } else if (color.b > color.g && color.b > color.r) {
-        generatedColors.bColors.push(color)
+        generatedColors.bColors.push(color);
       } else {
-        generatedColors.sameColors.push(color)
+        generatedColors.sameColors.push(color);
       }
 
-
-      i++
+      i++;
 
       if (i < 11) {
-        analyzeColor(newColors)
+        analyzeColor(newColors);
       }
     },
 
-    500
-  )
-  console.log(i)
+    500,
+  );
+
   console.log(generatedColors);
 }
 
 function formatNewColor(color) {
-  const newColor = `rgb(${color.r}, ${color.g}, ${color.b})`
-  colorBoxEl.style.backgroundColor = newColor
-  colorBoxEl.classList.add('app__color-box--changeColor')
+  const newColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
+  colorBoxEl.style.backgroundColor = newColor;
+  colorBoxEl.classList.add("app__color-box--changeColor");
 }
 
-customColorEl.addEventListener('click', () => getRadioColors("red", "green", "blue"))
-changeColorEl.addEventListener('click', () => analyzeColor(undefined));
-
-
-function getRadioColors(...arguments) {
-  const newColors = {};
-  for (let arg of arguments) {
-    let radioValue = document.querySelector(`input[name="${arg}"]:checked`).value.split('-').map(Number);
-    newColors[arg] = radioValue
-  }
-  analyzeColor(newColors);
-}
-
-
-
-
-
-
-
+changeColorEl.addEventListener("click", () => analyzeColor(undefined));
 
 
 
 // reference for new router - S-375684137
+
+const colorElements = Array.from(document.getElementsByTagName('input'));
+const newModifiedColors = {}
+
+
+colorElements.forEach(function (elem) {
+  elem.addEventListener("input", function (e) {
+    const newLow = parseInt(e.target.value) - 10;
+    const newHigh = parseInt(e.target.value) + 10;
+    newModifiedColors[elem.name] = [newLow, newHigh];
+  })
+})
+
