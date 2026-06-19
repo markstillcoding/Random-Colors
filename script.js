5a4e2dffed0e999d1beb0807dc564d9a49d58747
Mark Cates <markstillcoding@gmail.com>
2026-06-19 17:34:26 -0500

    added ability to create new colors and have them show up dynamically in a grid with the rgb values centered in each color box.  also added event listeners to each one with event delegation

--- script.js
+++ script.js
@@ -1,127 +1,182 @@
-const changeColorEl = document.querySelector(".app__btn");
-const colorBoxEl = document.querySelector(".app__color-box");
-const customColorEl = document.querySelector(".custom-color");
+const changeColorEl = document.querySelector('.app__btn')
+const customColorEl = document.querySelector('.custom-color')
+const colorGridEl = document.querySelector('.color-grid')
 const newModifiedColors = {}
-
-const generatedColors = {
-  rColors: [],
-  gColors: [],
-  bColors: [],
-  sameColors: [],
-};
+const generatedColors = []
+let timer = 1
 
 function createNewColor() {
-  let r = Math.floor(Math.random() * 255) + 1;
-  let g = Math.floor(Math.random() * 255) + 1;
-  let b = Math.floor(Math.random() * 255) + 1;
+  let r = Math.floor(Math.random() * 255) + 1
+  let g = Math.floor(Math.random() * 255) + 1
+  let b = Math.floor(Math.random() * 255) + 1
 
   let newColor = {
     r,
     g,
-    b,
-  };
+    b
+  }
 
-  return newColor;
-}
-
-function createChosenColor(color) {
-  let r = Math.floor(Math.random() * (color.red[1] - color.red[0]) + color.red[0]);
-  let g = Math.floor(Math.random() * (color.green[1] - color.green[0]) + color.green[0]);
-  let b = Math.floor(Math.random() * (color.blue[1] - color.blue[0]) + color.blue[0]);
-
-  let newColor = {
-    r,
-    g,
-    b,
-  };
   return newColor
 }
 
-let i = 1;
+function createChosenColor(color) {
+  let r = Math.floor(
+    Math.random() * (color.red[1] - color.red[0]) + color.red[0]
+  )
+  let g = Math.floor(
+    Math.random() * (color.green[1] - color.green[0]) + color.green[0]
+  )
+  let b = Math.floor(
+    Math.random() * (color.blue[1] - color.blue[0]) + color.blue[0]
+  )
 
-function analyzeColor(newColors) {
-  let color;
-  if (newColors !== undefined) {
-    console.log(newColors)
-    color = createChosenColor(newColors)
-    console.log(color)
-  } else {
-    color = createNewColor();
+  let newColor = {
+    r,
+    g,
+    b
   }
 
+  return newColor
+}
 
+function createColorShades(color) { }
 
+// function getRedColors(color) {
+//   let obj = { rColors: [{ r: color.r }, { g: color.g }, { b: color.b }] }
+//   generatedColors.push(obj);
+// }
+// function getGreenColors(color) {
+//   let obj = { gColors: [{ r: color.r }, { g: color.g }, { b: color.b }] }
+//   generatedColors.push(obj);
+// }
+// function getBlueColors(color) {
+//   let obj = { bColors: [{ r: color.r }, { g: color.g }, { b: color.b }] }
+//   generatedColors.push(obj);
+// }
+// function getSameColors(color) {
+//   let obj = { sameColors: [{ r: color.r }, { g: color.g }, { b: color.b }] }
+//   generatedColors.push(obj);
+// }
+
+function createColorArray(newColors) {
+  if (newColors !== undefined) {
+    color = createChosenColor(newColors)
+  } else {
+    color = createNewColor()
+  }
 
   setTimeout(
     () => {
-      formatNewColor(color);
-      if (color.r === color.g && color.r > color.b) {
-        generatedColors.rColors.push(color);
-      }
+      formatNewColor(color)
+      createColorBox(color)
+      generatedColors.push(color)
+      timer++
 
-      if (color.r === color.b && color.r > color.g) {
-        generatedColors.rColors.push(color);
-      }
-
-      if (color.b === color.g && color.b > color.r) {
-        generatedColors.bColors.push(color);
-      }
-
-      if (color.r > color.g && color.r > color.b) {
-        generatedColors.rColors.push(color);
-      }
-
-      if (color.g > color.b && color.g > color.r) {
-        generatedColors.gColors.push(color);
-      }
-
-      if (color.b > color.g && color.b > color.r) {
-        generatedColors.bColors.push(color);
-      }
-
-      generatedColors.sameColors.push(color);
-
-
-      i++;
-
-      if (i < 11) {
-        analyzeColor(newColors);
+      if (timer < 11) {
+        createColorArray(newColors)
       }
     },
 
-    500,
-  );
+    200
+  )
+
+}
+
+function analyzeColor(newColors) {
+  if (newColors !== undefined) {
+    color = createChosenColor(newColors)
+  } else {
+    color = createNewColor()
+  }
+
+  setTimeout(() => {
+    formatNewColor(color)
+
+    if (color.r === color.g && color.r > color.b) {
+      getRedColors(color)
+    }
+
+    if (color.r === color.b && color.r > color.g) {
+      getRedColors(color)
+    }
+
+    if (color.b === color.g && color.b > color.r) {
+      getBlueColors(color)
+    }
+
+    if (color.r > color.g && color.r > color.b) {
+      getRedColors(color)
+    }
+
+    if (color.g > color.b && color.g > color.r) {
+      getGreenColors(color)
+    }
+
+    if (color.b > color.g && color.b > color.r) {
+      getBlueColors(color)
+    }
+
+    if ((color.b === color.g) === color.r) {
+      getSameColors(color)
+    }
+
+    timer++
+
+    if (timer < 11) {
+      analyzeColor(newColors)
+    }
+  }, 200)
+}
+
+function createColorBox(color) {
+  const newColorDiv = document.createElement('div')
+  const newColorSpan = document.createElement('span')
+  newColorSpan.classList.add('app__color-box-span')
+  newColorDiv.classList.add('app__color-box')
+  newColorSpan.textContent = `rgb(${color.r},${color.g},${color.b})`
+  newColorDiv.style.backgroundColor = `rgb(${color.r},${color.g},${color.b})`
+  colorGridEl.append(newColorDiv)
+  newColorDiv.append(newColorSpan)
 }
 
 function formatNewColor(color) {
-  const newColor = `rgb(${color.r}, ${color.g}, ${color.b})`;
-  colorBoxEl.style.backgroundColor = newColor;
-  colorBoxEl.classList.add("app__color-box--changeColor");
+  const newColor = `rgb($ {
+      color.r
+    }
+
+    , $ {
+      color.g
+    }
+
+    , $ {
+      color.b
+    }
+
+  )`
+  return newColor
 }
 
-changeColorEl.addEventListener("click", () => analyzeColor(undefined));
-customColorEl.addEventListener('click', () => analyzeColor(newModifiedColors));
-
+changeColorEl.addEventListener('click', () => createColorArray(undefined))
+customColorEl.addEventListener('click', () =>
+  createColorArray(newModifiedColors)
+)
 
 // reference for new router - S-375684137
 
-const colorElements = Array.from(document.getElementsByTagName('input'));
-
-
+const colorElements = Array.from(document.getElementsByTagName('input'))
 
 colorElements.forEach(function (elem) {
-  elem.addEventListener("input", function (e) {
-    const newLow = parseInt(e.target.value) - 10;
-    const newHigh = parseInt(e.target.value) + 10;
-    newModifiedColors[elem.name] = [newLow, newHigh];
+  elem.addEventListener('input', function (e) {
+    const newLow = parseInt(e.target.value) - 10
+    const newHigh = parseInt(e.target.value) + 10
+    newModifiedColors[elem.name] = [newLow, newHigh]
   })
 })
 
 
-
-
-
-
-
-
-
+colorGridEl.addEventListener('click', e => {
+  const target = e.target.closest(".app__color-box");
+  if (target) {
+    console.log(e.target)
+  }
+})
